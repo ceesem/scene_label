4b904b6935fc0c9efba1753c0d11d7098057f3bb
Casey Schneider-Mizell <caseysm@gmail.com>
2019-05-17 11:21:29 -0700

    install requirements

---
+++ README.txt
@@ -0,0 +1,5 @@
+Simple class for adding labels to a collection of scenes composed of vtk actors.
+
+Use however you want.
+
+-Casey Schneider-Mizell
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='scene_label',
+      version='0.1',
+      description='Simple vtk scene labeler',
+      url='http://www.github.com/ceesem/scene_label/',
+      author='Casey Schneider-Mizell',
+      author_email='caseysm@gmail.com',
+      license='MIT',
+      packages=['scene_label'],
+      )
+
